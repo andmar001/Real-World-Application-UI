e4fa465e49636ee178cab51ad3f4c511a5be5925
andmar001 <andrademaresantonio00@gmail.com>
2024-03-05 10:46:56 -0600

    getall - usado pipe para suscripcion a observables

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -3,6 +3,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.models';
 import { CategoryService } from '../services/category.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-category',
@@ -14,7 +15,9 @@ export class AddCategoryComponent implements OnDestroy {
   model: AddCategoryRequest;
   private addCategorySuscription?: Subscription;
 
-  constructor( private _categoryService: CategoryService ) {
+  constructor( private _categoryService: CategoryService,
+               private _router: Router
+  ) {
     this.model = {
       name: '',
       urlHandler: ''
@@ -25,7 +28,7 @@ export class AddCategoryComponent implements OnDestroy {
     this.addCategorySuscription = this._categoryService.addCategory(this.model)
       .subscribe({
         next: ( response ) => {
-          console.log('This was a success!');
+          this._router.navigate(['/admin/categories']);
         },
         error: (err) => {
           console.error(err);
