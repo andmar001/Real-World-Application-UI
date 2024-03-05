9fd6f0eb81571763827d56683c09d2f97affcfcf
andmar001 <andrademaresantonio00@gmail.com>
2024-03-05 10:08:39 -0600

    ADD Listado de categories - getall()

--- src/app/features/category/category-list/category-list.component.ts
+++ src/app/features/category/category-list/category-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
+
+  categories?:Category[];
+
+  constructor( private _categoryService:CategoryService ) { }
+
+  ngOnInit(): void {
+    this._categoryService.getAllCategories()
+      .subscribe({
+        next: ( response ) => {
+          this.categories = response;
+        }
+      })
+  }
 
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://localhost:44367/api'
+};
