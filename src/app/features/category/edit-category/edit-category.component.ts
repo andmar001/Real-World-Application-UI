cc3d9df127811a5eabd0f896ffb73950640ee83e
andmar001 <andrademaresantonio00@gmail.com>
2024-03-11 23:21:41 -0600

    Edit component - Activated Router

--- src/app/features/category/edit-category/edit-category.component.ts
+++ src/app/features/category/edit-category/edit-category.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-category',
   templateUrl: './edit-category.component.html',
   styleUrls: ['./edit-category.component.css']
 })
-export class EditCategoryComponent {
+export class EditCategoryComponent implements OnInit, OnDestroy {
+
+  id: string | null = null;
+  paramSuscription?: Subscription;
+
+  constructor( private _route:ActivatedRoute ) { }
+
+  ngOnInit(): void {
+    this.paramSuscription = this._route.paramMap.subscribe({
+      next: (params) => {
+        this.id = params.get('id');
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.paramSuscription?.unsubscribe();
+  }
 
 }
