c3f0c311b27de8d7326c379602f407c2f19ed245
andmar001 <andrademaresantonio00@gmail.com>
2024-03-11 23:41:34 -0600

    Desplegar una sola categoria - paramMap

--- src/app/features/category/edit-category/edit-category.component.ts
+++ src/app/features/category/edit-category/edit-category.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-edit-category',
@@ -11,15 +13,33 @@ export class EditCategoryComponent implements OnInit, OnDestroy {
 
   id: string | null = null;
   paramSuscription?: Subscription;
+  category?:Category;
 
-  constructor( private _route:ActivatedRoute ) { }
+  constructor( private _route:ActivatedRoute, private _categoryService:CategoryService ) { }
 
   ngOnInit(): void {
     this.paramSuscription = this._route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
+
+        if (this.id){
+          //get the data from the server
+          this._categoryService.getCategoryById(this.id)
+            .subscribe({
+              next: (response) => {
+                this.category = response;
+              },
+              error: (error) => {
+                console.log(error);
+            }
+          })
+        }
       }
     });
+  }
+
+  onFormSubmit(){
+    console.log(this.category);
   }
 
   ngOnDestroy(): void {
--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -19,4 +19,8 @@ export class CategoryService {
   getAllCategories():Observable<Category[]>{
     return this._htpp.get<Category[]>(`${environment.apiBaseUrl}/categories`);
   }
+
+  getCategoryById( id:string ):Observable<Category>{
+    return this._htpp.get<Category>(`${environment.apiBaseUrl}/categories/${id}`);
+  }
 }
