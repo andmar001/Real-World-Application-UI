3657595ef1ed179178ced9d84d6b352d6de972bc
andmar001 <andrademaresantonio00@gmail.com>
2024-03-23 20:53:28 -0600

    Data Binding modelo de blog

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
 
 @Component({
   selector: 'app-add-blogpost',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-blogpost.component.css']
 })
 export class AddBlogpostComponent {
+  model:AddBlogPost;
+
+  constructor(){
+    this.model ={
+      title: '',
+      shortDescription: '',
+      urlHandle: '',
+      content: '',
+      featuredUImageUrl: '',
+      author: '',
+      publishedDate: new Date(),
+      isVisible: true
+    }
+  }
+
+  onFormSubmit(){
+    console.log(this.model);
+  }
 
 }
---
+++ src/app/features/blog-post/models/add-blog-post.model.ts
@@ -0,0 +1,10 @@
+export interface AddBlogPost {
+  title: string;
+  shortDescription: string;
+  content: string;
+  featuredUImageUrl: string;
+  urlHandle: string;
+  author: string;
+  publishedDate: Date;
+  isVisible: boolean;
+}
